6c771732eecce71e3cba7c2a6a663ac22e53d549
Masihdev <masihullahmassudi85@gmail.com>
2023-06-22 10:06:57 +0200

    add reactiveForms and json-server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -36,7 +37,8 @@ import { MatSelectModule } from '@angular/material/select';
     MatDatepickerModule,
     MatNativeDateModule,
     MatRadioModule,
-    MatSelectModule
+    MatSelectModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/emp-add-edit/emp-add-edit.component.ts
+++ src/app/emp-add-edit/emp-add-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'emp-add-edit',
@@ -6,6 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./emp-add-edit.component.scss']
 })
 export class EmpAddEditComponent {
-
+  empForm: FormGroup;
   education: string[] = ['Matric', 'Diploma', 'Intermediate', 'Graduate', 'Post Graduate'];
+
+  constructor(private formBuilder: FormBuilder) {
+    this.empForm = this.formBuilder.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      dob: '',
+      gender: '',
+      education: '',
+      company: '',
+      experience: '',
+      package: ''
+    });
+  }
+
+
+  onSubmit() {
+    if(this.empForm.valid) {
+      console.log(this.empForm.value);
+    }    
+  }
 }
